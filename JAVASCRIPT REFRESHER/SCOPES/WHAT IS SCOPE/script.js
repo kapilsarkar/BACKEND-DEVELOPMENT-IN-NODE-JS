b1b8487faff6998a8a9376953473959dae1894f2
kapilsarkar <k.sarkar81@gmail.com>
2024-01-18 18:24:55 +0530

    somepartsadded

---
+++ JAVASCRIPT REFRESHER/SCOPES/WHAT IS SCOPE/script.js
@@ -0,0 +1,44 @@
+var teacher = "Kapil";
+function fun() {
+  var teacher = "Piku";
+  console.log(teacher);
+}
+
+function gun() {
+  var student = "JOHN CENA";
+  console.log(student);
+}
+
+fun();
+gun();
+console.log(teacher);
+
+//block scope and functional scope
+
+
+{
+  var x = 10
+}
+
+console.log(x)
+
+{
+  let y= 10
+}
+console.log(y)
+
+function fun() {
+  var k = 10
+}
+
+fun()
+
+function gun(){
+  console.log(z)
+ var z= 20
+}
+
+gun()
+
+
+
