32d2a949727ae851346af4a1eed9bf7f49e11f36
kapilsarkar <k.sarkar81@gmail.com>
2024-01-20 17:05:24 +0530

    somepartsadded

--- JAVASCRIPT REFRESHER/HOF AND OTHER FUNCTIONS/script.js
+++ JAVASCRIPT REFRESHER/HOF AND OTHER FUNCTIONS/script.js
@@ -16,4 +16,47 @@ f(10,function exec(){
     console.log('I am an Expression passed to an Higher Order Function')
 })
 
-//arrays are also 
+//arrays are also custom object in js
+//index of the element is the key and the element itself is the value
+//["abc","def","ghi"]
+
+//map function
+
+//higher order functions that depends and operates ojn other functions
+
+//higher order function takes another function as an argument or they return a function and then executes its logic
+//map is a higher order function available with arrays --> it takes a function as an argument example function f and it returns an array in which every value is actually populated by calling function f  with original array element as argument
+
+//map internally iterates/loops over every element of the given original array pass the element  in the argument function and then store the returned value inside an array.
+
+function square(el){
+  return el*el  //returns square
+}
+
+
+
+const arr = [1,2,3,4,5]
+
+const result = arr.map(square)  //square is a function passed as an  argument
+
+
+console.log(result)
+
+function cube(x){
+  return x*x*x
+}
+
+const cubeArr = arr.map(cube)
+console.log(cubeArr)
+
+function evenOrOdd(x){
+  if(x%2 == 0){
+    return "Even"
+  }
+  else{
+    return "Odd"
+  }
+}
+
+const evenOddResult = arr.map(evenOrOdd)
+console.log(evenOddResult)
