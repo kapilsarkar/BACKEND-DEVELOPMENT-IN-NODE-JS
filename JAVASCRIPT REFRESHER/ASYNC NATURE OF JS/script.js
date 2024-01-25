141be7489c49a32b77d30ae8f51deeec53da3f42
kapilsarkar <k.sarkar81@gmail.com>
2024-01-25 20:50:50 +0530

    somepartsadded

--- JAVASCRIPT REFRESHER/ASYNC NATURE OF JS/script.js
+++ JAVASCRIPT REFRESHER/ASYNC NATURE OF JS/script.js
@@ -7,7 +7,7 @@
 console.log("Hi 1")
 
 for(let i =0 ; i<=1000000;i++){
-    //some task
+    //some task                     // THIS CODE IS NATIVE OR KNOWN TO JAVASCRIPT
 }
 
 console.log("hi 2")
@@ -15,7 +15,30 @@ console.log("hi 2")
 console.log("HI KAPIL")
 
 setTimeout(function(){
-   console.log("HI WAIT")
+   console.log("HI WAIT")  //SETTIMEOUT IS NOT NATIVE OR KNOWN OR IT IS NOT PRIORITY OF JAVASCRIPT TO JAVASCRIPT
 },5000)
 
 console.log("H1 SARKAR")
+
+function timeConsumingByLoop(){
+    console.log("Loop Starts")
+    for(let i =0;i< 1000000;i++){
+         //some task
+    }
+    console.log("Loop Ends")
+}
+
+function timeConsumingByRunTimeFeature(){
+    console.log("Starting Timer")
+
+    setTimeout(function exec(){
+        console.log("Completed the Timer")
+    },2000)
+}
+
+console.log("Hi Piku")
+timeConsumingByLoop()
+timeConsumingByRunTimeFeature()
+timeConsumingByLoop()
+
+console.log("By Piku")
