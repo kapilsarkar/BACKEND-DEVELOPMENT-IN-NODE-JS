c3c0c594b74eca98c0ff1cd8bc8ec7a888158e3a
kapilsarkar <k.sarkar81@gmail.com>
2024-05-30 17:09:08 +0530

    added

--- MONGOOSE-NODEJS-PROJECT-2/index.js
+++ MONGOOSE-NODEJS-PROJECT-2/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const uri = "mongodb://127.0.0.1/shop";
+//const uri = "mongodb://127.0.0.1/shop"; //For connecting with the local server.
+
+const uri = "mongodb+srv://ksarkar81:12345@cluster0.fpbpqh8.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"  //For connecting with Atlas server
 
 mongoose.connect(uri)  //Connection with the server
 
@@ -18,6 +20,17 @@ const productsSchema = new mongoose.Schema({
     isFeatured:Boolean,
 })
 
+//? 2nd step while inserting the data
+const data1 = {
+   name: "Designer Handbag2",
+   company: "64c23350e32f4a51b19b923a",
+   price: 2466,
+   colors: ["#000000", "#cc6600", "#663300"],
+   image: "/images/product-handbag.png",
+   category: "64c2342de32f4a51b19b9250",
+   isFeatured: true,
+ };
+
 //We need to create a model.
 //With the help of this model we can perform all the queries in the database.
 
@@ -25,10 +38,34 @@ const Product = new mongoose.model('Product',productsSchema) //Connection with t
 
 const main = async()=>{
  try{
+     //Find Documents:
 
     const data = await Product.find({price:{$gt:1200}})
     console.log(data)
+    //Insert Documents:
 
+   //  await Product.insertMany(data1)
+   //  const data2 = await Product.find({price:{$eq:2466}})
+   //  console.log(data2)
+
+
+    //Update Documents:
+
+     await Product.findOneAndUpdate(
+      {name:"Designer Handbag2",price:2466},
+      {$set:{price:2499}}
+
+      //? 3 Delete query
+
+   //  await Product.findOneAndDelete({ name: "Designer Handbag2", price: 2499 });
+   //  const data = await Product.find({
+   //    name: "Designer Handbag2",
+   //    price: 2499,
+   //  });
+
+   )
+   const data2 = await Product.find({price:{$eq:2499}})
+   console.log(data2)
  }
  catch(err){
     console.log(err)
