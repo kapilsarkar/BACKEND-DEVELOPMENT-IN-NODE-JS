dd7db1eabbd20608ea37c35557f2d9594e8884c7
kapilsarkar <k.sarkar81@gmail.com>
2024-06-05 08:37:45 +0530

    someprtasadded

--- NodeJS Bootcamp/NODE JS PRACTICE/DAY-04-ASSIGNMENT/server.js
+++ NodeJS Bootcamp/NODE JS PRACTICE/DAY-04-ASSIGNMENT/server.js
@@ -11,3 +11,38 @@
 //Step-1 : Creating a Database: `use mystudent`
 //Step-2 : Create a collection named students : `db.createCollection("students")`
 //Step-3 : MongoDB query : `db.students.insertOne({name:'John',age:'23',grade:'A+'})`
+
+//Solution a) `db.students.insertOne({name:'John',age:'23',grade:'A+'})`
+
+//b) Write a MongoDB query to update the "age" field of a document in the "employees" collection with the name "John" to 30.
+
+//Step-1 : Create a collection named employees : `db.createCollection("employees")`
+//Step-2 : Insert Data into the Field : `db.employees.insertOne({name:'Kapil',age:33})` and `db.employees.insertOne({name:'john',age:48})`
+
+//Step-3 : Update the  Field : `db.employees.updateOne({'name':'john' },{$set:{age:30}})`
+
+//Solution b) `db.employees.updateOne({'name':'john' },{$set:{age:30}})`
+
+//c) Write a MongoDB query to delete a document from the "products" collection with the name "Product A."
+
+//Step-1: Create a collection named products : ` db.createCollection('products')`
+//Step-2 : Insert Data into the field : `db.products.insertOne({name:'Product A',price:999})`
+
+//Step-3 : Delete the document : `db.products.deleteOne({name:'Product A'})`
+
+//Solution c) `db.products.deleteOne({name:'Product A'})`
+
+//d) Write a MongoDB query to retrieve all documents from the "orders" collection where the total amount is greater than $100.
+
+//Step-1 : Create a collection named orders : ` db.createCollection('orders')`
+//Step-2 : Insert Data into the field  : `db.orders.updateOne({name:'AppleMobile',{$set:{totalAmount:'$600'}}})`
+
+//Step-3 : retrieve all documents from the "orders" collection where the total amount is greater than $100 :
+//`db.orders.find({totalAmount:{$gt:'$100'}})`
+
+//Solution d) `db.orders.find({totalAmount:{$gt:'$100'}})`
+
+
+
+
+
